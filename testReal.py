5b22d2818e1ee42e5d5e020d806368cc4f20b599
Camron Bartlow <csbartlow99@gmail.com>
2019-02-10 09:29:50 -0600

    Added documentation

--- testReal.py
+++ testReal.py
@@ -13,22 +13,15 @@ i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
+
+# Global variables
 firsties = True
-#SPI connection:
-# from digitalio import DigitalInOut, Direction
-# spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-# csag = DigitalInOut(board.D5)
-# csag.direction = Direction.OUTPUT
-# csag.value = True
-# csm = DigitalInOut(board.D6)
-# csm.direction = Direction.OUTPUT
-# csm.value = True
-# sensor = adafruit_lsm9ds1.LSM9DS1_SPI(spi, csag, csm)
 
 # Main loop will read the acceleration, magnetometer, gyroscope, Temperature
 # values every second and print them out.
 
 while True:
+    #Priority for thresholds to determine when to play the sound
     priority = ""
     # Read acceleration, magnetometer, gyroscope, temperature.
     accel_x, accel_y, accel_z = sensor.acceleration
@@ -39,13 +32,14 @@ while True:
 
     accels = [accel_x, accel_y, accel_z]
     gyros = [gyro_x, gyro_y, gyro_z]
-    # temp = sensor.temperature
-    # Print values.
+    # Print values to terminal for debugging and analysis
     print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
     print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
     print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
-    # print('Temperature: {0:0.3f}C'.format(temp))
     print('Avg gyro: {0:0.3f}'.format(gyro_avg))
+
+    # Determine max value to set priortization
+    # Uses absolute values so multidirectional swings are recognized
     minVal = round(min(list(accels)), 3)
     maxVal = round(max(list(accels)), 3)
     if abs(maxVal) > abs(minVal):
@@ -63,13 +57,16 @@ while True:
     elif maxVal == round(abs(accel_z), 3):
         priority = "Z"
         gyro_priority = gyro_avg - abs(gyro_z)
+
+    # Print Priority stats for debugging
     print('Priority: ' + priority + ' at ' + str(maxVal) + str(round(abs(accel_z))))
 
+    # Swing is based on gyro_avg and a given threshold
     gyro_avg = gyro_priority
-
     if gyro_avg >= 275 and not firsties:
         GPIO.output(17, GPIO.HIGH)
-    # Delay for a second.
+
+    # Delay for a quarter-second.
     time.sleep(0.25)
     GPIO.output(17, GPIO.LOW)
     firsties = False
